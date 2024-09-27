9d71e278b62d8b34eb475b1f41e8661e7f1a4b29
Rounak Gour <rounakgour110@gmail.com>
2024-09-27 18:49:26 +0530

    card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import Grid from '@mui/material/Grid2';
+import { Box } from '@mui/material';
 import Main from './pages/main';
+import Card from './components/card';
 
 function App() {
   return (
-    <div>
-     <Main />
-    </div>
+    <Box>
+      <Main />
+      <Grid container sx={{padding:'50px 20px'}}>
+        <Grid size={{ xs: 12, sm: 12, md: 4, lg: 4, xl: 3 }} className='div-center'>
+          <Card />
+        </Grid>
+        <Grid size={{ xs: 12, sm: 12, md: 4, lg: 4, xl: 3 }} className='div-center'>
+          <Card />
+        </Grid>
+        <Grid size={{ xs: 12, sm: 12, md: 4, lg: 4, xl: 3 }} className='div-center'>
+          <Card />
+        </Grid>
+        <Grid size={{ xs: 12, sm: 12, md: 4, lg: 4, xl: 3 }} className='div-center'>
+          <Card />
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import Grid from '@mui/material/Grid2'
+import React from 'react';
+import Ngo from '../assets/ngo.jpg'
 
 export default function Card() {
   return (
-    <div>card</div>
+    <Grid className="main-card">
+      <div className='card-image-container'>
+        <img src={Ngo} alt="Banner" className='card-image' />
+      </div>
+      <div className='card-inner-box'>
+        Help Afreen Who Can’t Talk Or Swallow A Morsel Of Food Because Of A Rare Condition
+      </div>
+      <div className='card-bottom-box'>
+        <div className='card-button div-center'>Share</div>
+        <div className='card-button div-center'>Donate</div>
+      </div>
+    </Grid>
   )
 }
