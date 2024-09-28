b7a676d4d5982e815b092d232759e97844a78675
Rounak Gour <rounakgour110@gmail.com>
2024-09-29 00:39:54 +0530

    mobile view

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,39 +1,120 @@
-import React from 'react'
+import * as React from 'react';
+import Grid from '@mui/material/Grid2';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
-export default function Header() {
-  return (
-    <section class="navbar-navbarWrapper">
-      <nav class="navbar-navbar navbar-flexItemCenter navbar-fixedTop navbar-pageScrolled">
-        <a class="navbar-navbarBrand" to="/" href="/">
-        
-        </a>
-        <div class="navbar-flexItemCenter navbar-linksWrapper">
-          <ul class="navbar-navbarNav">
-            <li id="exploreCampaigns" role="presentation">
-              <a class=" ">
-                Explore Campaigns
-              </a>
-            </li>
-            <li id="requestACampaign" role="presentation">
-              <a class="navbar-newLink">
-                Start A Campaign
-              </a>
-            </li>
-            <li id="donateMonthly" role="presentation">
-              <button type="button" class="button-gradient-btn  navbar-btnMonthly button-gradient-gradient">
-                Donate Monthly
-              </button>
-            </li>
-          </ul>
-          <ul class="navbar-navbarNav">
-            <li class="account-menu-item-userDropdown" role="presentation">
-              <a class="">
-                Join Us / Login
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </section>
-  )
+const drawerWidth = 240;
+const navItems = ['Home', 'Category', 'Testimonial'];
+
+function Header(props) {
+    const { window } = props;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen((prevState) => !prevState);
+    };
+
+    const drawer = (
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+            <Typography variant="h6" sx={{ my: 2 }}>
+                MUI
+            </Typography>
+            <Divider />
+            <List>
+                {navItems.map((item) => (
+                    <ListItem key={item} disablePadding>
+                        <ListItemButton sx={{ textAlign: 'center' }}>
+                            <ListItemText primary={item} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+                <button type="button" class="button-gradient-btn  navbar-btnMonthly button-gradient-gradient">
+                    Donate Monthly
+                </button>
+            </List>
+        </Box>
+    );
+
+    const container = window !== undefined ? () => window().document.body : undefined;
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar component="nav">
+                <Toolbar>
+                    <Grid sx={{
+                        display: { xs: 'flex', sm: 'flex', md: 'none', lg: 'none', xl: 'none' },
+                        justifyContent: 'space-between',
+                        alignItem: 'center',
+                        width: '100%',
+                    }}>
+                        <Grid
+                            sx={{ mr: 2, display: { sm: 'none' } }}
+                        >
+                            <button type="button" class="button-gradient-btn  navbar-btnMonthly button-gradient-gradient">
+                                LOGO
+                            </button>
+                        </Grid>
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            edge="end"
+                            onClick={handleDrawerToggle}
+                            sx={{ mr: 2, display: { sm: 'none' } }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                    </Grid>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                    >
+                        LOGO
+                    </Typography>
+                    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                        {navItems.map((item) => (
+                            <Button key={item} sx={{ color: '#425691' }}>
+                                {item}
+                            </Button>
+                        ))}
+                        <button type="button" class="button-gradient-btn  navbar-btnMonthly button-gradient-gradient">
+                            Donate Monthly
+                        </button>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            <nav className='navbar-navbar'>
+                <Drawer
+                    container={container}
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: { xs: 'block', sm: 'none' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+            </nav>
+        </Box>
+    );
 }
+
+export default Header;
